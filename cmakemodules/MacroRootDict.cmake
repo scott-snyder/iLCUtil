d685f2c025a90ba03a421afcf9b7aabce9b761ab
sss <sss@karma>
2026-01-09 15:47:41 -0500

    Fix for building in ABS_SYMLINK mode.
    cling tries to load the PCM file for a library in the same directory as the
    library, after resolving symlinks.
    But the libraries are built in the build/PACK/lib directory and the PCM
    files in build/PACK/rootdict.  They both get symlinked in inst/lib,
    but that doesn't help.  Fix by putting a link to the PCM< in build/PACK/lib.

--- cmakemodules/MacroRootDict.cmake
+++ cmakemodules/MacroRootDict.cmake
@@ -170,6 +170,7 @@ MACRO( GEN_ROOT_DICT_SOURCE _dict_src_filename )
         OUTPUT  ${_dict_src_file} ${_dict_hdr_file}
         COMMAND mkdir -p ${ROOT_DICT_OUTPUT_DIR}
         COMMAND ${ROOT_CINT_WRAPPER} -f "${_dict_src_file}" -c ${ROOT_DICT_CINT_DEFINITIONS} ${_dict_includes} ${ROOT_DICT_INPUT_HEADERS}
+        COMMAND ln -sf ${_dict_hdr_file} ${LIBRARY_OUTPUT_PATH}
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         DEPENDS ${ROOT_DICT_INPUT_HEADERS}
         COMMENT "generating: ${_dict_src_file} ${_dict_hdr_file}"
